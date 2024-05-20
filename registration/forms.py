5b90eea17bf819c9f9e148fbda310658e8333bbf
Ekaterina1239 <ekaterinasohrannaya2005@gmail.com>
2024-05-20 21:40:08 +0500

    qw

--- registration/forms.py
+++ registration/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
-from .models import CustomUser
+from .models import Staff
+
 
 class RegistrationForm(forms.ModelForm):
+
     password = forms.CharField(widget=forms.PasswordInput)
-    groups = forms.CharField()
 
     class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password', 'role', 'groups']
+        model = Staff
+        fields = "__all__"
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
--- registration/models.py
+++ registration/models.py
@@ -1,47 +1,26 @@
 from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
-# Create your models here.
-# class CustomUser(AbstractUser):
-#
-#     ROLES = (
-#         ('teacher', 'Teacher'),
-#         ('admin', 'Admin'),
-#         ('student', 'Student'),
-#         ('tester', 'Tester'),
-#     )
-#     groups = models.ManyToManyField(
-#         'auth.Group',
-#         related_name='customuser_set',
-#         blank=True,
-#         verbose_name='groups',
-#         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-#     )
-#     user_permissions = models.ManyToManyField(
-#         'auth.Permission',
-#         related_name='customuser_set',
-#         blank=True,
-#         verbose_name='user permissions',
-#         help_text='Specific permissions for this user.',
-#     )
-#     role = models.CharField(max_length=20, choices=ROLES)
-
 
 class StudentGroups(models.Model):
-    pass
+    title = models.CharField(max_length=255)
 
-class TeacherProfile(models.Model):
+
+class Staff(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    appointment = models.CharField(max_length=10)
+
+
+class TeacherProfile(Staff):
     bio = models.TextField()
     subjects_taught = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to='teacher_profiles/', null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user
 
 
-class StudentProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE )
-    group = models.ForeignKey(StudentGroups)
+class StudentProfile(Staff):
+    group = models.ForeignKey(StudentGroups, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='student_profile/', null=True, blank=True)
 
